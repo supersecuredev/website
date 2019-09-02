2ff996bd06d0142b054545943f1083c9b30a78d2
John Doe <supersecuredev@outlook.com>
2019-09-03 00:02:50 +0100

    Upgraded logins to use sessions

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 
 
+import json
 import os
 
 app = Flask(__name__)
 
-credentials = {'admin': 'password'}
+global g_credentials
+g_credentials = {}
 
+def load_credentials(file_path):
+	try:
+		with open(file_path, "r") as f:
+			credentials = json.load(f)
+			return credentials
+	except Exception as e:
+		raise e
 
 def generate_cat_art(words):
 	return """
@@ -30,13 +39,18 @@ def reset_session():
 def home():
 	if 'cat_art' not in session:
 		session['cat_art'] = generate_cat_art("meow!")
+
 	return render_template('cat.html', cat_art=session['cat_art'])
 
 @app.route('/login', methods=['GET', 'POST'])
-def do_admin_login():
+def do_login():
+	if 'credentials' not in session:
+		global g_credentials
+		session['credentials'] = g_credentials
+
 	if request.method == 'POST':
 
-		if request.form['username'] in credentials and credentials[request.form['username']] == request.form['password']:
+		if request.form['username'] in session['credentials'] and session['credentials'][request.form['username']] == request.form['password']:
 			session['logged_in'] = True
 			session['username'] = request.form['username']
 		else:
@@ -58,8 +72,11 @@ def cat():
 @app.route("/logout")
 def logout():
 	session['logged_in'] = False
+	session.pop("username", None)
 	return home()
 
 if __name__ == "__main__":
 	app.secret_key = os.urandom(12)
+	global g_credentials
+	g_credentials = load_credentials("default.json")
 	app.run(debug=True,host='0.0.0.0', port=4000)
